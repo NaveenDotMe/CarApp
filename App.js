8aefa4c436eb4f5f8df20af58b8f519a746cb044
Brock-99 <elmatadordelmar99@gmail.com>
2022-10-04 09:22:21 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import LoginPage from './Screen/LoginPage'
+
+export default function App() {
+  return (
+    <View>
+      <LoginPage/>
+    </View>
+  )
+}
